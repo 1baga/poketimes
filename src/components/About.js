32b62b7d4a209311d90c725ca8f281c0dfb48dc5
masho <mafulun4real@gmail.com>
2018-09-10 23:31:47 +0100

    First Commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow'
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi dolor molestias, ipsum hic sapiente sit nobis fugiat tempora ex error quis accusamus ut odio explicabo dolorem fugit tenetur vitae nisi.</p>
+    </div>
+  )
+}
+
+export default Rainbow(About)
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = (props) => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi dolor molestias, ipsum hic sapiente sit nobis fugiat tempora ex error quis accusamus ut odio explicabo dolorem fugit tenetur vitae nisi.</p>
+    </div>
+  )
+}
+
+export default Contact
